f85ab225b26e9f9ee4c95d8ee6323e4b294fea0c
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-27 12:59:31 -0400

    started working on the generateMarkdown function

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-
-
-
-
-
-
-
 // TODO: Create an array of questions for user input
 const questions = [
   {
