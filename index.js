4d4d1808d72dd694d660bed5a479302cc700eab4
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-27 12:58:25 -0400

    started working on adding the questions to the array

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+
+
+
+
+
+
+
 // TODO: Create an array of questions for user input
 const questions = [
   {
